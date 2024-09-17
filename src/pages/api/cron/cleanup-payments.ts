cb64c00ae8f06bee645ae28655919122d2a48f1b
amhedcb <amhed.herrera@coinbase.com>
2024-09-17 18:47:57 -0400

    Readding auth header check

--- src/pages/api/cron/cleanup-payments.ts
+++ src/pages/api/cron/cleanup-payments.ts
@@ -2,10 +2,11 @@ import { cleanupOldPayments } from '@helpers/cleanup-payments';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
-  // const authHeader = request.headers.get('authorization');
-  // if (!process.env.CRON_SECRET || authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return response.status(401).json({ success: false });
-  // }
+  const authHeader = request.headers['authorization'];
+  if (!process.env.CRON_SECRET || authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return response.status(401).json({ success: false });
+  }
+
   const deletedCount = await cleanupOldPayments();
   console.log(`Cleaned up ${deletedCount} old payment transactions`);
 
