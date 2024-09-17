cdf081412a9a615e6519db182457fb590e14fc6c
amhedcb <amhed.herrera@coinbase.com>
2024-09-17 19:09:44 -0400

    ff

--- src/pages/api/submitPaymentTx/index.ts
+++ src/pages/api/submitPaymentTx/index.ts
@@ -3,10 +3,17 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
-      res.status(201).json({ message: 'Dummy Endpoint, not yet implemented' });
+      res
+        .status(201)
+        .json({
+          message: 'Dummy Endpoint, not yet implemented',
+          txHash: '0x566bbc200b9d1ad32019ad26e4682b9d36cd645b6a1a58257a67f0047f70b8ca',
+        });
     } catch (error) {
       console.error('Error storing payment:', error);
-      res.status(500).json({ message: 'Error storing payment transaction params' });
+      res.status(500).json({
+        message: 'Error storing payment transaction params',
+      });
     }
   } else {
     res.setHeader('Allow', ['POST']);
