7e3aa9dc2a4af2bc15b889d4090a5f2d4a76009f
amhedcb <amhed.herrera@coinbase.com>
2024-09-17 18:45:42 -0400

    Remove experimental

--- src/pages/api/cron/cleanup-payments.ts
+++ src/pages/api/cron/cleanup-payments.ts
@@ -1,10 +1,15 @@
 import { cleanupOldPayments } from '@helpers/cleanup-payments';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export const config = {
-  runtime: 'experimental-edge',
-};
-
-export default async function handler() {
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  // const authHeader = request.headers.get('authorization');
+  // if (!process.env.CRON_SECRET || authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return response.status(401).json({ success: false });
+  // }
   const deletedCount = await cleanupOldPayments();
   console.log(`Cleaned up ${deletedCount} old payment transactions`);
+
+  response
+    .status(200)
+    .json({ success: true, message: `Cleaned up ${deletedCount} old payment transactions` });
 }
